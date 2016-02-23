c8114fe5ae1c660e3a04c9325f225cf6071eb692
Jordan Healy <jordan.healy36@mail.dcu.ie>
2016-02-23 17:17:46 +0000

    Create Infinite game mode

--- app/src/main/java/com/example/healyj36/quizapp/MainActivity.java
+++ app/src/main/java/com/example/healyj36/quizapp/MainActivity.java
@@ -1,69 +1,39 @@
 package com.example.healyj36.quizapp;
 
-import android.app.ListActivity;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-
-public class MainActivity extends ListActivity {
-    Intent intent; // TODO delete. maybe not needed
-    TextView questionId; // TODO delete. maybe not needed
-    ListView lvQuestions;
-
-    DBFunc dbFunc = new DBFunc(this);
-
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        try {
-            dbFunc.createDatabase();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void goToAllQuestions(View view) {
+        Intent viewAllQues = new Intent(MainActivity.this, AllQuestions.class);
 
-        ArrayList<HashMap<String, String>> listQuestions = dbFunc.getAllQuestions();
+        // final int result = 1; // signal
 
-        ArrayList<String> allQuestionNames = dbFunc.getAllQuestionNames();
-        if(listQuestions.size() != 0){
-            lvQuestions = getListView();
+        // call activity to run and don't expect data to be sent back
+        startActivity(viewAllQues);
 
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                    this,
-                    android.R.layout.simple_list_item_1,
-                    android.R.id.text1,allQuestionNames
-            );
+        // call activity to run and retrieve data back
+        // startActivityForResult(viewAllQues, result);
+    }
 
-            lvQuestions.setAdapter(adapter);
+    public void goToInfiniteGameMode(View view) {
+        Intent viewInfinite = new Intent(MainActivity.this, InfiniteModeStart.class);
 
-            lvQuestions.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                    // TODO open next page with question and options
-                    Intent viewQues = new Intent(MainActivity.this, ViewQuestion.class);
+        // final int result = 1; // signal
 
-                    // final int result = 1; // signal
+        // call activity to run and don't expect data to be sent back
+        startActivity(viewInfinite);
 
-                    String questionName = (String) parent.getItemAtPosition(position);
-
-                    viewQues.putExtra("questionKey", questionName);
-
-                    // call activity to run and don't expect data to be sent back
-                    startActivity(viewQues);
-
-                    // call activity to run and retrieve data back
-                    // startActivityForResult(viewQues, result);
-                }
-            });
-        }
+        // call activity to run and retrieve data back
+        // startActivityForResult(viewInfinite, result);
     }
 }
+
