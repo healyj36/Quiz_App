302f7d6b2f68e4811230d06671d3a958bf33ba50
Jordan Healy <jordan.healy36@mail.dcu.ie>
2016-03-09 09:31:27 +0000

    Add Google Play Sign-in

--- app/src/main/java/com/example/healyj36/quizapp/MainActivity.java
+++ app/src/main/java/com/example/healyj36/quizapp/MainActivity.java
@@ -5,11 +5,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void goTo1v1(View view) {
